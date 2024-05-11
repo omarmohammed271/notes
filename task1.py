2c2826a309204883808f9ac1001d6f3fbe89cd14
moooooora <omarmohammed271@gmail.com>
2024-05-11 22:01:18 +0300

    Added Task1

---
+++ task1.py
@@ -0,0 +1,227 @@
+# output
+# print('Hello','Omar',5)
+# print('Hello Omar')
+
+# input
+
+
+
+
+# print('Hello','Omar')
+# print('Hello'+' Omar')
+# print('Hello',5)
+# print(10+5)
+# () ** */ +-
+
+# to remove in terminal
+    # - clear
+    # -cls
+
+# print('Hello',input('Enter your name: '),'and your age is',input('Enter your age :'))
+
+# Variables 
+# name = 'Radwa'
+# age = 25
+# # print('Hello',name,'your age is :',age)
+# name = 'Omar'
+# print(name)
+# print(name)
+# programming 100% 100 20% pass interview
+# 1hr 2hr
+# name = input('Enter your name: ')
+# age = input('Enter your age: ')
+
+# print('Hello',name,'and age is ',age)
+
+# data Types
+# str ---->string 'Hello'
+# int ----> integer  0 123456
+# float ---> float  0.12315 5.5
+# bool ---> boolean   True False
+# list ----> ['omar',28,0.211321]
+# tuple----> ('omar',28,0.211321)
+# dict ----> dictionary {'key':'value}
+
+# name = 'Omar'  # str
+# age = 28 #int
+# salary = 456546.1315646 #float
+# is_active = True   # bool
+
+# print(type(name))
+# print(type(age))
+# print(type(salary))
+# print(type(is_active))
+
+# name = input('Enter your name: ')    #str
+# age = input('Enter your age: ')        #int
+
+# print(type(name))
+# print(type(age))
+
+# print('Hello',name,'and age is ',age)
+# number1 = float(input('Enter num1: '))
+# number2 = float(input('Enter num2: '))
+
+# print(type(number1))
+# print(type(number2))
+
+# # operations 
+# print(number1 + number2)
+# print(number1 - number2)
+# print(number1 * number2)
+# print(number1 / number2)
+# print(number1 ** number2)  #2**3
+# print(number1 % number2)  
+# 10 %2 
+# 10/2 = 5.0
+
+
+# print(23%2)
+# 23 / 2 = 11.5 ---> % ----1
+# 24/2 --- > 12.0 ---> 0 
+
+# Genius AI  
+# Python
+
+# variables  ------> Data type
+        # list  
+name = 'Omar'
+age = 28
+salary = 5465465
+names = ['Omar','Rawda','Mohamed','Edward'] #-----> str
+numbers = [1,2,3,4,56,897897] #-----> int
+# x = ['Omar',65464654,0.2116,True,[False,8798798,'Ahmed']]
+# list
+# employee1 = ['Omar',28,464564564]
+# mutable  
+
+# tuple
+# days = ('sat','sun','mon')
+# can't delete item or update or add
+# speed immutable
+
+# dictionary  ------> dict 
+
+# website ====> template dict  
+# convension
+# employee1 = ['Omar',28,464564564]
+# print(employee1)
+# employee = {
+#     #key      Value
+#     'name'  : 'Omar',
+#     'age'   :   28 ,
+#     'salary' : 46545646
+# }
+# print(employee)
+
+
+
+
+
+
+# condition
+# if  
+# if condition ==True :
+#     block of coding
+# else:
+#     block of coding
+
+#  >        <      ==     >=      <=    != (not equal)
+
+
+# x = 10
+# if x != 10:
+#     print(True)
+# else:
+#     print(False)    
+
+# name = 'ahmed'
+
+# if name == 'omar':
+#     print(True)
+# else:
+#     print(False)
+# names = ['Omar','Radwa','Mohamed','Edward']
+
+# if 'said' in names:
+
+#     print(True)
+# else:
+#     print(False)
+# name = 'ahmed'
+
+# if 'a' in name:
+#     # block of code 
+#     print(True)
+
+# else:
+#     print(False)
+
+# condition
+# if  
+# if condition ==True :
+#     block of coding
+# elif condition ==True:
+    # block of coding
+# elif condition ==True:
+    # block of coding
+# elif condition ==True:
+    # block of coding
+# elif condition ==True:
+    # block of coding
+# elif condition ==True:
+    # block of coding
+# else:
+#     block of coding
+
+# x = 50
+
+# if x > 50:
+#     print('x greater than 50')
+# elif x == 50:
+#     print('x equal 50')
+# elif x < 50 :
+#     print('x < 50')  
+# else:
+#     print(False)      
+
+# list ---> 
+# tuple ---> 
+# dict ----> 
+# if elif else----->    
+
+
+
+# x = 50
+
+# if x >= 50:
+#     print('x greater than 50')
+# if x == 50:
+#     print('x equal 50')
+# if x <= 50 :
+#     print('x < 50')  
+# else:
+#     print(False)  
+
+# x = 50
+
+# if x >= 50:
+#     print('x greater than 50')
+# elif x == 50:
+#     print('x equal 50')
+# elif x <= 50 :
+#     print('x < 50')  
+# else:
+#     print(False)  
+
+
+# 4 interview  
+    # - intro   (Market  Hr ) 
+    # - problem solving interview (coding) ----> X
+    # - Technical interview   approval
+    # - Manager Team Leader 
+
+num = [1,2,3,4,65]
+x,y = num
+print(x)
+print(y)
